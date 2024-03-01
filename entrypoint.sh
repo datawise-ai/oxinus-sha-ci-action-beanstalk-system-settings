488e536db2cd09450b479014d174892d2042d1d0
Panos Georgantas <panos@georgantas.info>
2024-03-01 20:35:06 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ SOURCE_ROOT=/beanstalk
 DESTINATION_ROOT=${GITHUB_WORKSPACE:-/github/workspace}
 
 find /beanstalk
-cp -af ${SOURCE_ROOT}/ ${DESTINATION_ROOT}/
+cp -af ${SOURCE_ROOT}/.platform ${DESTINATION_ROOT}/
+cp -af ${SOURCE_ROOT}/nginx.conf ${DESTINATION_ROOT}/
 echo lalalal
 ls -al ${DESTINATION_ROOT}/
 ls -al ${DESTINATION_ROOT}/nginx*
